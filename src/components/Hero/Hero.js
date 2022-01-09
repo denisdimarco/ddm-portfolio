7d3dee7ac0f05e6bcf6c1e9e60eda043e0c9b02b
denisdimarco <denisdimarcoleda@gmail.com>
2022-01-08 23:47:00 -0300

    Added Home Info

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,22 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+      <LeftSection>
+          <SectionTitle main center>
+              Welcome to <br />
+              Denis Di Marco's Portfolio
+          </SectionTitle>
+          <SectionText>
+              I'm a full-stack developer.
+          </SectionText>
+          <Button onClick={() => window.open("https://www.linkedin.com/in/denisdimarco/", "_blank")}>
+              Learn More
+          </Button>
+      </LeftSection>
+
+  </Section>
 );
 
 export default Hero;
